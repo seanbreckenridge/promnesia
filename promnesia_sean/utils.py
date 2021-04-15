4ad94d80e404f22d5af209e651049e85ee8faa7c
Sean Breckenridge <seanbrecke@gmail.com>
2021-04-14 17:36:26 -0700

    update http-only urlextraction

--- promnesia_sean/utils.py
+++ promnesia_sean/utils.py
@@ -4,7 +4,7 @@ from promnesia.common import extract_urls, Url
 
 
 def _naive_is_url(u: str) -> bool:
-    return u.startswith("http") or u.startswith("www")
+    return u.startswith("http") or u.startswith("www.")
 
 
 # extract urls that look like web links, ignore anything else
