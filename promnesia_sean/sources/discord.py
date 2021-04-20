b1120acb023500843723c6da5808af7452dc15b9
Sean Breckenridge <seanbrecke@gmail.com>
2021-04-19 20:00:04 -0700

    discord: link/description creation moved
    added that directly as properties to the
    discord_data module instead

--- promnesia_sean/sources/discord.py
+++ promnesia_sean/sources/discord.py
@@ -11,29 +11,12 @@ def index() -> Results:
     from my.discord import messages
 
     for m in messages():
-        # extract any URLs from content
-        # hmm - extract URLs from attachments.
+        # hmm - extract URLs from attachments?
         # Probably not very useful unless I extract info from them with url_metadata or something
-        urls = extract_urls(m.content)
-
-        if len(urls) == 0:
-            continue
-
-        cid = m.channel.channel_id
-        # no server info, probably a PM
-        if m.channel.server is None:
-            desc = m.channel.name or f"message ({cid})"
-            link = f"{BASE}/channels/@me/{cid}/{m.message_id}"
-        else:
-            s = m.channel.server
-            # has a server object, in a server
-            desc = f"{s.server_name} - #{m.channel.name}"
-            link = f"{BASE}/channels/{s.server_id}/{cid}/{m.message_id}"
-
-        for u in urls:
+        for u in extract_urls(m.content):
             yield Visit(
                 url=u,
                 dt=m.timestamp,
                 context=m.content,
-                locator=Loc(title=desc, href=link),
+                locator=Loc(title=m.channel.description, href=m.link),
             )
